06872b1506e89a3ab985584a9482a4daecc127a0
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-04-04 13:43:13 +0400

    feat: make it possible to make a guaranteed subscription to events
    There was a problem that if for some reason
    the service was not available or something with rabbitMQ,
    There was a problem that if for some reason
    the service was not available or something with rabbitMQ,
    emails may not have been sent and the queue in RabbitMQ may have just cleared out.
    It was decided to create a separate queue for notification-service
    and link it to exchange from auth-service.
    We also added a confirmation of message receipt in RabbitMQ
    and resending the message in case of failure,
    which also gives a guarantee that the user will be get messages via email
    
    !!! Note:
    For correct operation, you must manually configure queues
    and connect them to exchange in the RabbitMQ interface

--- src/email/events/user.events.controller.ts
+++ src/email/events/user.events.controller.ts
@@ -1,7 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { MyLogger } from '../../logger/my-logger.service';
-import { PasswordResetRequestedDto, UserCreatedDto } from '../dto';
 import { EmailService } from '../email.service';
 
 @Controller()
@@ -11,33 +10,51 @@ export class UserEventsController {
     private readonly logger: MyLogger,
   ) {}
 
-  @EventPattern('user_created')
-  async userCreatedEvent(dto: UserCreatedDto) {
-    this.logger.log({
-      method: 'userCreatedEvent',
-      log: `received data for email: ${dto.email}`,
-    });
+  @EventPattern()
+  async handleAllEvents(@Payload() dto: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
 
-    await this.emailService.sendEmailConfirmation(dto);
-  }
+    try {
+      switch (dto.eventType) {
+        case 'user_created':
+          this.logger.log({
+            method: 'user_created event',
+            log: `received data for email: ${dto.email}`,
+          });
+          await this.emailService.sendEmailConfirmation(dto);
+          break;
+        case 'password_reset_requested':
+          this.logger.log({
+            method: 'password_reset_requested event',
+            log: `received data for email: ${dto.email}`,
+          });
+          await this.emailService.sendPasswordResetLink(dto);
+          break;
+        case 'email_changed':
+          this.logger.log({
+            method: 'email_changed event',
+            log: `received data for email: ${dto.email}`,
+          });
+          await this.emailService.sendEmailConfirmation(dto);
+          break;
+        default:
+          this.logger.log({
+            method: 'handleAllEvents',
+            log: `Unknown event type: ${dto.eventType}`,
+          });
+      }
 
-  @EventPattern('password_reset_requested')
-  async passwordResetRequestedEvent(dto: PasswordResetRequestedDto) {
-    this.logger.log({
-      method: 'passwordResetRequestedEvent',
-      log: `received data for email: ${dto.email}`,
-    });
+      // Confirmation of successful message processing
+      channel.ack(originalMsg);
+    } catch (error) {
+      this.logger.error({
+        method: `handleAllEvents-${dto.eventType}`,
+        error: `Error processing message: ${error.toString()}`,
+      });
 
-    await this.emailService.sendPasswordResetLink(dto);
-  }
-
-  @EventPattern('email_changed')
-  async emailChangedEvent(dto: any) {
-    this.logger.log({
-      method: 'emailChangedEvent',
-      log: `received data for email: ${dto.email}`,
-    });
-
-    await this.emailService.sendEmailConfirmation(dto);
+      // Resend the message to the queue
+      channel.nack(originalMsg, false, true);
+    }
   }
 }
